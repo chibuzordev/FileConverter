b7d60f60d15686b816311e0fab4890f378e0e9f7
Chibuzor Nwachukwu <58276420+chibuzordev@users.noreply.github.com>
2025-04-14 18:11:40 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="data-converter",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "xlsxwriter"
+    ],
+    entry_points={
+        "console_scripts": [
+            "dataconvert=cli:main"
+        ]
+    },
+    author="Your Name",
+    description="A file conversion framework for CSV/Excel/JSON data transformation.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/data-converter",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
